f716f4cf8076eccdd9fff54f0dadaa0e8c60d06a
Arikalp <sankalpkrsaini@gmail.com>
2025-03-29 20:20:14 +0530

    eyes comp

--- src/components/Balls.jsx
+++ src/components/Balls.jsx
@@ -1,22 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Balls = () => {
-  return (
-    <>
-    <div className='balls-container'>
-        <img src="src\assets\balls img.jpg" alt="" />
+    const [deg, setdeg] = useState(0);
 
-        
-    </div>
-    <div className='balls-text'>
-            <div className='ballone'>
-                <h1>Play</h1>
-            </div>
-            <div className='balltwo'>
-                <h1>Play</h1>
-            </div>
-        </div>
+    useEffect(() => {
+        const handleMouseMove = (dets) => {
+            let x = dets.clientX;
+            let y = dets.clientY;
 
-    </>
-  )
-}
+            let delx = x - window.innerWidth / 2;
+            let dely = y - window.innerHeight / 2;
+
+            let angle = Math.atan2(dely, delx);
+            let degree = angle * 180 / Math.PI;
+            setdeg(degree-180);
+        };
+
+        window.addEventListener("mousemove", handleMouseMove);
+        return () => window.removeEventListener("mousemove", handleMouseMove);
+    }, []);
+
+    return (
+        <>
+            <div className="balls-container">
+                <img src="src/assets/balls img.jpg" alt="" />
+            </div>
+            <div className="eyes">
+                <div className="ballone">
+                    <div className="inner-ball">
+                        <div
+                            className="innerline"
+                            style={{
+                                transform: `rotate(${deg}deg)`,
+                            }}
+                        >
+                            <div className="white-ball"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="balltwo">
+                    <div className="inner-ball">
+                        <div
+                            className="innerline"
+                            style={{
+                                transform: `rotate(${deg}deg)`,
+                            }}
+                        >
+                            <div className="white-ball"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
